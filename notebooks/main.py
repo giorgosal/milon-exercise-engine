0c84d3c058679c454807970ac15e82e334ce622e
Stelios Georgaras <steliosgp13@gmail.com>
2026-03-01 19:29:12 +0200

    add main for testing

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,33 +1,19 @@
-from milon_engine.exercises import PushUp, Exercise
-from milon_engine.core.pose_estimator import PoseEstimator
-from milon_engine.core.visualizer import Visualizer
-from milon_engine.core import FrameProcessor
 import cv2
+from milon_engine import FrameProcessor, PoseEstimator, Visualizer, Squat, load_config
 
+config = load_config("squat")
+exercise = Squat(config, fps=30.0, calibration_path="outputs/calibration/squat.yaml")
+processor = FrameProcessor(exercise, PoseEstimator(), Visualizer())
 
-config = "config/pushup.yaml"
-pose_estimator = PoseEstimator()
-# exercise = PushUp(config)
-exercise = Exercise(config)
-# exercise.load_calibration("pushup_calib.yaml")
-visualizer = Visualizer(
-    mp_drawing=pose_estimator.mp_drawing, mp_pose=pose_estimator.mp_pose
-)
-
-processor = FrameProcessor(exercise, pose_estimator, visualizer)
-
-# Run
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
+while True:
     ret, frame = cap.read()
     if not ret:
         break
-
-    # Single responsibility: process frame
     annotated = processor.process_frame(frame)
-
-    cv2.imshow("Counter", annotated)
-    if cv2.waitKey(10) & 0xFF == ord("q"):
+    cv2.imshow("Milon", annotated)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
 cap.release()
 cv2.destroyAllWindows()
